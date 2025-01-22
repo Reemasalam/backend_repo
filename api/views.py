58167088874ba30930dd8601c2614047ad078bbf
Reemasalam <reemasalam6554@gmail.com>
2025-01-22 15:49:00 +0530

    fix

---
+++ api/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets, filters
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author', 'genre']
